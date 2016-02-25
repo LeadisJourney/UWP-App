2281aa045fec533fb4504bc59591012ca2d8d6e9
Aksamyt <alexandre@szymocha.com>
2016-02-25 14:40:55 +0900

    Going from F# to C++/CX

---
+++ VS2015_1/CParser/Cparser.h
@@ -0,0 +1,29 @@
+﻿#pragma once
+
+namespace Pfm = ::Platform;
+
+namespace Cparser
+{
+  public enum class TokenType
+  {
+    None
+  };
+
+  public value struct Token sealed
+  {
+    uint16 begin;
+    uint16 end;
+    TokenType type;
+  };
+
+  public delegate void NewTokenParsed(Token token);
+
+  public ref class CParser sealed
+  {
+    std::vector<std::pair<wchar_t, int>> text;
+    ~CParser();
+  public:
+    CParser(Pfm::String ^text);
+    event NewTokenParsed ^NewTokenParsed;
+  };
+}
--- VS2015_1/Leadis Journey/EditableSourceCode.cs
+++ VS2015_1/Leadis Journey/EditableSourceCode.cs
@@ -3,6 +3,8 @@ using Windows.UI.Text;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
+using System.Collections.Concurrent;
+using System.Runtime.InteropServices;
 
 // The Templated Control item template is documented at http://go.microsoft.com/fwlink/?LinkId=234235
 
@@ -20,13 +22,21 @@ namespace Leadis_Journey
         readonly Color colorComment = Color.FromArgb(0xFF, 0x57, 0xA6, 0x4A);
 
         private RichEditBox xEditBox;
-        private CParser cParser = new CParser();
+        private ConcurrentBag<Cparser.Token> bag;
+        private Cparser.CParser cparser;
 
         public EditableSourceCode()
         {
             this.DefaultStyleKey = typeof(EditableSourceCode);
             this.ApplyTemplate();
             this.Background = new SolidColorBrush(colorBackground);
+            this.cparser = new Cparser.CParser("test");
+            this.cparser.NewTokenParsed += Cparser_NewTokenParsed;
+        }
+
+        private void Cparser_NewTokenParsed(Cparser.Token token)
+        {
+            this.bag.Add(token);
         }
 
         protected override void OnApplyTemplate()
@@ -39,55 +49,6 @@ namespace Leadis_Journey
             this.xEditBox.FontFamily = new FontFamily("Consolas");
             this.xEditBox.Background = new SolidColorBrush(Colors.Transparent);
             this.xEditBox.Foreground = new SolidColorBrush(colorText);
-            this.xEditBox.TextChanging += XEditBox_TextChanging;
-            this.xEditBox.KeyDown += XEditBox_KeyDown;
-        }
-
-        private void XEditBox_KeyDown(object sender, KeyRoutedEventArgs e)
-        {
-            var xEditBox = sender as RichEditBox;
-            if (e.Key == Windows.System.VirtualKey.Tab)
-            {
-                xEditBox.Document.Selection.TypeText("  ");
-                e.Handled = true;
-            }
-        }
-
-        private void XEditBox_TextChanging(RichEditBox sender, RichEditBoxTextChangingEventArgs args)
-        {
-            var document = sender.Document;
-            string text;
-            document.GetText(0, out text);
-            document.GetRange(0, text.Length).CharacterFormat.ForegroundColor = colorText;
-            var tokens = this.cParser.Parse(text);
-            foreach (var token in tokens)
-            {
-                var range = document.GetRange(token.Start, token.End);
-                switch (token.Type)
-                {
-                    case TokenType.Keyword:
-                        range.CharacterFormat.ForegroundColor = colorKeyword;
-                        break;
-                    case TokenType.Directive:
-                        range.CharacterFormat.ForegroundColor = colorDirective;
-                        break;
-                    case TokenType.LitNumber:
-                        range.CharacterFormat.ForegroundColor = colorLitNumber;
-                        break;
-                    case TokenType.LitString:
-                        range.CharacterFormat.ForegroundColor = colorLitString;
-                        break;
-                    case TokenType.Macro:
-                        range.CharacterFormat.ForegroundColor = colorMacro;
-                        break;
-                    case TokenType.Comment:
-                        range.CharacterFormat.ForegroundColor = colorComment;
-                        break;
-                    default:
-                        break;
-                }
-            }
-            document.ApplyDisplayUpdates();
         }
     }
 }
